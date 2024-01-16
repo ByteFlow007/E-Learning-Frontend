1b978df8bb7db71d1eb95649b4aab85296da5327
ByteFlow <byteflow007@gmail.com>
2024-01-17 00:19:38 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
-function Navbar() {
+import {Link} from 'react-router-dom';
+function Navbar({isLoggedIn,isAdmin}) {
   return (
-    <div className="text-3xl font-bold underline">Navbar</div>
+    <div>
+      <h1>Byteflow007</h1>
+    if(isLoggedIn){
+      (isAdmin)?
+      <div>
+        <a>Create Course</a>
+        <a>MY Courses</a>
+        <a>Students</a>
+        <a>Logout</a>
+      </div> :
+      <div>
+        <a>My Learnings</a>
+        <a>Courses</a>
+        <a>Students</a>
+        <a>Log out</a>
+      </div>
+    }
+    else{
+      <div>
+        <a>Courses</a>
+        <a>Logout</a>
+      </div>
+    }
+      
+      
+    </div>
   )
 }
 
