1d57c74451c384d8e4af90d9cfccc8993212e3c2
ByteFlow <byteflow007@gmail.com>
2024-01-21 15:34:51 +0530

    components added

---
+++ src/components/Signin/SignInSuccessMessage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function SignInSuccessMessage() {
+  return (
+    <div
+      role="alert"
+      className="alert alert-success z-10 w-fit fixed right-0 bottom-0"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="stroke-current shrink-0 h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>Signin Successful</span>
+    </div>
+  );
+}
+
+export default SignInSuccessMessage;
